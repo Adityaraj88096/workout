eedc1981c0b92e2e209d815799015f3df5b0aad6
Adityaraj88096 <Adityaraj88096@gmail.com>
2022-10-21 23:14:14 +0530

    Bugs fixed

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const connectDB = require('./db/db')
-const exerciseRouter = require('./routes/exercise.router');
+const connectDB = require('../db/db');
+const exerciseRouter = require('../routes/exercise.router');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 connectDB();
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
