9481ac9fc6cea4b7c0df62ff4e3b432538312d3e
Adityaraj88096 <Adityaraj88096@gmail.com>
2022-10-22 08:47:53 +0530

    For deployment

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const URI = process.env.MONGO_URI
+const URI = process.env.MONGO_URI || 'mongodb+srv://aditya:aditya@cluster0.xeeoygw.mongodb.net/ExerciseDB?retryWrites=true&w=majority'
 
 const connectDB = async () => {
     try{
